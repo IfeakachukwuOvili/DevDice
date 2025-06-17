4025b16ef7d5eab21de2e0e09196e8051d8c7bea
Ovili <ovilifeakachukwu@gmail.com>
2025-06-17 11:47:08 -0500

    I put the wrong url lol

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: 'https://devdiceserver.onrender.com/',
+  origin: 'https://devdice.onrender.com',
   credentials: true,
 }));app.use(express.json());
 app.use('/users', userRouter);
